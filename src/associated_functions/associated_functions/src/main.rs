cd79083e88ce83b8124f88aac65eb77e0587e647
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-16 15:47:24 +0000

    Add main.rs

--- src/associated_functions/associated_functions/src/main.rs
+++ src/associated_functions/associated_functions/src/main.rs
@@ -1,12 +1,13 @@
 mod person;
 
-use person::{Person, Status};
+use person::person::{Person};
 
 fn main() {
-    let mut person = Person::new("Alice, "1990-01-03);
-    println!("Name: {}, Status {:?}", person.name, person.status);
+    let mut person = Person::new("Alice", "1990-01-01");
+
+    println!("Person: {}, Date of Bird: {}, Status: {:?}", person.name, person.dob, person.status);
 
     person.kill();
 
-    println!("Name: {}, is now: {:?}, person.name, person.status");
+    println!("Person after kill: {}, Status: {:?}", person.name, person.status);
 }

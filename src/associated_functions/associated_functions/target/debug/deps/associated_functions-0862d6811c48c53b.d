6e42f8d5a649cbf2dbbb8ac1bafb49ceb2d0e8b0
OliverKeefe <oliverkeefe@tutanota.com>
2025-03-16 15:48:01 +0000

    Add lib.rs

---
+++ src/associated_functions/associated_functions/target/debug/deps/associated_functions-0862d6811c48c53b.d
@@ -0,0 +1,9 @@
+/home/oliverkeefe/RustroverProjects/rust-for-n00bs/src/associated_functions/associated_functions/target/debug/deps/libassociated_functions-0862d6811c48c53b.rmeta: src/lib.rs src/person/mod.rs src/person/person.rs
+
+/home/oliverkeefe/RustroverProjects/rust-for-n00bs/src/associated_functions/associated_functions/target/debug/deps/libassociated_functions-0862d6811c48c53b.rlib: src/lib.rs src/person/mod.rs src/person/person.rs
+
+/home/oliverkeefe/RustroverProjects/rust-for-n00bs/src/associated_functions/associated_functions/target/debug/deps/associated_functions-0862d6811c48c53b.d: src/lib.rs src/person/mod.rs src/person/person.rs
+
+src/lib.rs:
+src/person/mod.rs:
+src/person/person.rs:
---
+++ src/associated_functions/associated_functions/target/debug/deps/associated_functions-26ff45726006722c.d
@@ -0,0 +1,7 @@
+/home/oliverkeefe/RustroverProjects/rust-for-n00bs/src/associated_functions/associated_functions/target/debug/deps/associated_functions-26ff45726006722c: src/main.rs src/person/mod.rs src/person/person.rs
+
+/home/oliverkeefe/RustroverProjects/rust-for-n00bs/src/associated_functions/associated_functions/target/debug/deps/associated_functions-26ff45726006722c.d: src/main.rs src/person/mod.rs src/person/person.rs
+
+src/main.rs:
+src/person/mod.rs:
+src/person/person.rs:
